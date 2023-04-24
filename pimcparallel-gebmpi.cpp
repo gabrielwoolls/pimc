09bb954ab4b74647fc51970fabab5a331b428c0a
Gabriel Woolls <beekby01@login26.chn>
2023-04-24 15:56:50 -0700

    first mpi attempt

---
+++ pimcparallel-gebmpi.cpp
@@ -0,0 +1,341 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <fstream>
+#include <cstdlib>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <random>
+#include <iomanip>
+#include <chrono>
+#include <utility> // for std::pair used in `thermo_sample`
+#include <omp.h>
+#include <tuple> // for std::pair used in `thermo_sample`
+#include <mpi.h>
+
+#define OMP_NUM_THREADS 12
+
+using namespace std;
+
+
+// these are used to generate random numbers later.
+int seed = 8;
+std::random_device rd;
+std::mt19937 e2( seed ? seed : rd());
+std::uniform_real_distribution<> dist(0., 1.);
+std::default_random_engine generator;
+
+int n = 500; // num of particles
+int M = 40; // num total timesteps
+int dim = 2; // num spatial dimensions
+
+double b = 3.0; // inverse temperature (name 'beta' gives a warning)
+double dt = b/M; // imaginary timestep (related to beta)
+
+double lambd = 6.0596; // hbar^2 / 2*m_He
+
+double L = 10.0; // box size is -L to L in dim dimensions
+
+int n_updates = 20000; // number of attempts at updating worldlines per step
+int acceptances = 0; // tracker of how many updates get accepted
+
+// OMP params
+int barrier_wait = 1; // How many updates to do independently before manual barrier
+
+
+//functions
+double V_ext(double**, int);
+double U(double***, double**, int, int, int);
+void beadbybead_T(double**, int, int);
+void beadbybead_A(double***, double**, int, int);
+double U_all_particles(double ***, int);
+std::tuple<double, double> thermo_sample(double***);
+
+
+int main(int argc, char* argv []) {
+    do_one_simulation();
+}
+
+
+int do_one_simulation() {
+    std::srand(time(NULL));
+    std::cout << std::fixed;
+    std::cout << std::setprecision(2);
+
+    // Set up OMP and MPI
+    omp_set_num_threads(OMP_NUM_THREADS);
+
+    auto start_time = std::chrono::steady_clock::now();
+
+    // make array of all particle worldlines:
+    // Q_ijk = (particle id i, time step j, dimension k)
+    // put this back in eventually but vectors are easier to debug
+    double*** Q = new double**[n];
+
+    #pragma omp parallel for
+        for(int i = 0; i < n; ++i){
+            Q[i] = new double*[M];
+            for(int j = 0; j < M; ++j){
+                Q[i][j] = new double[dim];
+            }
+        }
+
+    // // temporary vec of vec of vecs, easier to debug than pointers
+    // std::vector<std::vector<std::vector<double>>> Q(n,std::vector<std::vector<double>>(M,std::vector<double>(dim)));
+
+    // initialize particle positions somewhere in the box
+    double rand_q;
+    #pragma omp parallel for
+        for(int i = 0; i < n; ++i) {
+            for(int k = 0; k < dim; ++k) {
+                rand_q = L*(2.*dist(e2) - 1.); //pseudorand. number btwn -L and L
+                for(int j = 0; j < M; ++j) {
+                    Q[i][j][k] = rand_q;
+                }
+            }
+        }
+
+    // try to update particle worldlines
+
+
+    #pragma omp parallel
+    {
+        int my_id, num_threads;
+        my_id = omp_get_thread_num();
+        num_threads = omp_get_num_threads();
+
+
+        // Q_test will hold one particle's new trial worldline:
+        //std::vector<std::vector<double>> Q_test(M,std::vector<double>(dim));
+        // Each OpenMP thread needs its own `Q_test`, hence why it's after the `#pragma`
+        double** Q_test = new double*[M];
+        for(int i = 0; i < M; ++i){
+            Q_test[i] = new double[dim];
+        }
+        int t_upd;
+
+        // Each OMP thread goes through its assigned particles and updates them
+        // For now, split the particles evenly among threads. No reason to assume
+        // things won't be load-balanced since these are independent particles
+        int my_start, my_end;
+        my_start = my_id * n / num_threads;
+        my_end = (my_id + 1) * n / num_threads;
+        if (my_id == num_threads - 1) {my_end = n;}
+
+        // Each particle is updated n_updates times
+        for(int updates = 0; updates < n_updates; updates++) {
+
+            // Each thread goes through the particles it owns
+            for( int loc_particle = my_start; loc_particle < my_end; loc_particle++) {
+
+                //std::cout << "Thread " << my_id << " working on particle " << loc_particle << std::endl;
+
+                // fill in Q_test for all time slices
+                for(int t = 0; t < M; t++) {
+                    for(int k = 0; k < dim; k++) {
+                        Q_test[t][k] = Q[loc_particle][t][k];
+                    }
+                }
+
+                t_upd = floor(dist(e2) * M);
+                beadbybead_T(Q_test, loc_particle, t_upd); 
+
+                // A uses the Metropolis acceptance test to decide whether or not to accept the new worldline Q_test  
+                beadbybead_A(Q, Q_test, loc_particle, t_upd); 
+            }
+
+                if (updates % barrier_wait == 0) {
+                    #pragma omp barrier
+                    }
+
+            }
+    }
+    
+    // the energy & kinetic Cv computed from *this* Markov Chain initial condition
+    double energy_mc, cv_kin_mc;
+    tie(energy_mc, cv_kin_mc) = thermo_sample(Q);
+
+    std::cout << acceptances << " out of " << n*n_updates << " moves accepted." << std::endl;
+
+    auto end_time = std::chrono::steady_clock::now();
+
+    std::chrono::duration<double> diff = end_time - start_time;
+    double seconds = diff.count();
+
+    // Finalize
+    std::cout << "Simulation Time = " << seconds << " seconds for " << n << " particles.\n";
+
+
+    // write an output file
+    ofstream outFile("worldlines_parallel.txt");
+
+    // print array to file
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < M; j++) {
+            for (int k = 0; k < dim; k++) {
+                outFile << Q[i][j][k] << " ";
+            }
+            outFile << endl;
+        }
+        outFile << endl;
+    }
+
+    // close output file
+    outFile.close();
+
+
+    return 0;
+}
+
+
+
+// simple harmonic potential. modify for other dimensions + for pointers
+// pass it some Q[i] (one particle's worldline)
+double V_ext(double** Q, int t) {
+    double totV = 0.;
+    for (int k = 0; k < dim; k++) {
+        totV += 0.2*pow(Q[t][k],2);
+    }
+    return totV;
+}
+
+
+
+// lennard jones potential for two particles
+// needs Q[i] or Q_test (single particle worldline) and Q (for all other particles' worldlines for comparison)
+double U(double*** Q, double** Q_test, int i, int j, int t) {
+    double avg_sep = L/n;
+    double r2 = 0.;
+    for (int k = 0; k < dim; k++) {
+        r2 += pow(Q[i][t][k]-Q_test[t][k],2);
+    }
+    double r6 = pow(avg_sep / r2, 3);
+    return 4*(r6*r6 - r6);
+}
+
+// interparticle potential U for all particle pairs at time t
+double U_all_particles(double*** Q_ijk, int t){
+    double potential=0;
+    for (int a=0; a<n; a++){
+        for (int b=a+1; b<n; b++){
+            // add potential between particles (a,b)
+            potential += U(Q_ijk, Q_ijk[a], b, -1, t);
+        }
+    }
+    return potential;
+}
+
+
+// move a particle i at bead (time step) t according to gaussian dist, then write new pos.to Q_test
+void beadbybead_T(double** Q_test, int i, int t) {
+    double* r_mean = new double[dim];
+
+    for (int k = 0; k < dim; k++) {
+        // avg position of bead i+1 and bead i-1 ( modulos etc so we have PBC)
+        r_mean[k] = 0.5*(Q_test[(M+t-1)%M][k]+Q_test[(t+1)%M][k]);
+
+        // use a gaussian to update positions; it must have stdev lambda*dt and mean r_mean[k]
+        std::normal_distribution<double> distrib(r_mean[k],std::sqrt(dt*lambd));
+        Q_test[t][k] = distrib(generator);
+    }
+}
+
+
+
+// adjusts Q for a particle i and bead (time step) t
+void beadbybead_A(double*** Q, double** Q_test, int i, int t) {
+    double V_tot = 0;
+    // calculate total difference in energy between configuration in Q and that if we used Q_test
+    // aka V_tot = (energy of Q_test worldline) - (energy of current Q worldline)
+
+    // single particle contribution
+    V_tot += V_ext(Q_test, t) - V_ext(Q[i], t);
+
+    // sum to get two particle contribution
+    for (int l = 0; l < n; l++) {
+        if (l != i) {
+            V_tot += U(Q, Q_test, l, i, t) - U(Q, Q[i], l, i, t);
+        }
+    }
+
+    //if(i==0){std::cout << V_tot << std::endl;}
+    // instead of doing prob = min{1,e^(tau v' - v)}, just check if V_tot is positive
+    if (V_tot < 0) {
+        for (int k = 0; k < dim; k++) {
+            Q[i][t][k] = Q_test[t][k];
+            }
+        acceptances += 1;
+    }
+    else {
+        double prob = exp(-dt * V_tot);
+
+        // accept move only if a random number is lower than acceptance prob
+        if (dist(e2) < prob) {
+            for (int k = 0; k < dim; k++) {
+                    Q[i][t][k] = Q_test[t][k];
+                }
+
+            acceptances += 1;
+        }
+    }
+}
+
+std::tuple<double, double> thermo_sample(double*** Q_ijk){
+// Takes a set of worldlines `Q_ijk` (i->particle, j->time step, k->spatial dim)
+// and computes its contribution to the thermal energy U
+
+double kinetic = 0;
+double potential = 0;
+
+double u, cv = 0;
+
+for (int i=0; i<n; i++){
+    for (int k=0; k<dim; k++){
+        for (int j=0; j<M-1; j++){
+            // kinetic term (R_{mu+1}-R_mu)^2
+            kinetic += pow((Q_ijk[i][j+1][k]-Q_ijk[i][j][k]),2);
+            
+            // external harmonic potential
+            potential += V_ext(Q_ijk[i], j);
+
+            // interparticle LJ potential
+            potential += U_all_particles(Q_ijk, j);
+        }
+        // plus the periodic-boundary kinetic term connecting times 0 and M
+        kinetic += pow((Q_ijk[i,M-1,k]-Q_ijk[i,0,k]),2);
+    }
+}
+
+// kinetic *= M/(4*lambd*pow(b,2));
+// potential /= M;
+// return -(kinetic + potential);
+
+u = 0.5*dim*n*M/b -potential/M - kinetic*M/(4*lambd*pow(b,2));
+cv = -kinetic * M/(2*lambd*b);
+return std::make_tuple(u, cv);
+}
+
+// compute the heat capacity Cv from the 'kinetic' contribution + energy variance
+// u_array should be a list of thermal energies computed from different initial conditions at the same beta
+double get_heat_cap(double* u_array, double cv_kinetic, double beta){
+
+    int size = sizeof(u_array) / sizeof(u_array[0]);
+
+    double avg_u, avg_u2 = 0;
+    for (int i=0; i<size; i++){
+        avg_u += u_array[i];
+        avg_u2 += u_array[i]*u_array[i];
+    }
+
+    avg_u /= size;
+    avg_u2 /= size;
+
+    double Cv = (beta*beta)*(avg_u2-pow(avg_u,2));
+    Cv += cv_kinetic + 0.5*dim*n*M;
+
+    return Cv;
+}
+
+
+
